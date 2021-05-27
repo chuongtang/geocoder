dfeb208cac1a0c34820aae464b139e824811ec76
ChuongTang <chuong.tang@aol.com>
2021-05-27 16:24:31 -0600

    good formating

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import Map from './components/Map/Map'
-import IntroSection from './components/intro/Intro'
-// import ContactSection from './components/contact-section/ContactSection'
-import PlaceAutoComp from './components/PlaceAutoComp.jsx'
 
 
 import './App.css'
@@ -15,13 +12,9 @@ const Calgary = {
 } // Hard code YYC
 
 function App() {
- 
+
   return (
     <div className="App">
-      <div className='nonMap'>
-        <IntroSection />
-        <PlaceAutoComp />
-      </div>
       <Map location={Calgary} zoomLevel={16} />
     </div>
   )
--- src/components/Map/Intro.jsx
+++ src/components/Map/Intro.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import './intro.css'
 
 const Intro = () => {
   return (
